c6be1fbab3cca7315d08415f716f7e0efd8ccd73
Van-Hung <gogogo0646@gmail.com>
2021-07-08 21:43:47 +0700

    abc

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
+const initialState={};
 const middleware = [thunk];
 // const enhancer = compose(
 //   middleware, //middleware
@@ -19,6 +20,7 @@ const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
+  initialState,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__
