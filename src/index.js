11378fb33a959a0d7945d1ca7c297e3a517cbe9f
Van-Hung <gogogo0646@gmail.com>
2021-07-08 21:48:22 +0700

    bug apply

--- src/index.js
+++ src/index.js
@@ -9,18 +9,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const initialState={};
 const middleware = [thunk];
-// const enhancer = compose(
-//   middleware, //middleware
-//   //redux devtools
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-// const store = createStore(rootReducer, enhancer);
 
 const store = createStore(
   rootReducer,
-  initialState,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__
