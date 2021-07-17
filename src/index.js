9beb08b11f1e320958acbc2de411afaace3b6a5a
Van-Hung <gogogo0646@gmail.com>
2021-07-17 13:40:27 +0700

    xóa dev tool

--- src/index.js
+++ src/index.js
@@ -14,10 +14,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
+    applyMiddleware(...middleware)
   )
 );
 
